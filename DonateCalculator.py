1c3e6432fd4c0a8dc24054eccf074b65d7259e1e
catagain <shine.u.chen@gmail.com>
2023-12-02 21:44:38 +0800

    fix null SC bug

--- DonateCalculator.py
+++ DonateCalculator.py
@@ -10,6 +10,21 @@ import time
 
 TARGET = 500000
 
+# Check which country's dollar it is.
+def exchange(amount):
+    if amount[0:3] == 'HK$':
+        # it's HK dollar, exchange rate: 4
+        excahnge_amount = float(amount[3:].replace(',', '')) * 4
+    elif amount[0:4] == 'RMB¥':
+        # it's RMB
+        excahnge_amount = float(amount[4:].replace(',', '')) * 5
+    elif amount[0:3] == 'MYR':
+        excahnge_amount = float(amount[4:].replace(',', '')) * 6.72
+    else :
+        excahnge_amount = float(amount[1:].replace(',', ''))
+    # record the SC into history
+    return excahnge_amount
+
 def getDonateInfo(ID):
     browser = webdriver.Chrome()
 
@@ -26,9 +41,10 @@ def getDonateInfo(ID):
         count = 0
         for i in rec:
             if count == 1:
-                sumOfDonate += float(i[1:])
+                sumOfDonate += exchange(i)
             elif count == 2:
-                unique_chat.add(i)
+                if i != 'this is a null SC':
+                    unique_chat.add(i)
             if i == '---------------':
                 count = 0
             else:
@@ -84,34 +100,48 @@ def getDonateInfo(ID):
             break
 
         for i in SC:
-            #print(count, i.text)
+            # print(count, i.text)
             data = i.text.split('\n')
             print(data[0])
             print(data[1]) # this one is the amount
-            print(data[2])
+            if len(data) >= 3:
+                print(data[2])
 
-            if data[2] not in unique_chat:
-                # not in Set means this is a new SC
-                unique_chat.add(data[2])
+                if data[2] not in unique_chat:
+                    # not in Set means this is a new SC
+                    unique_chat.add(data[2])
 
-                # Check which country's dollar it is.
-                if data[1][0:3] == 'HK$':
-                    # it's HK dollar, exchange rate: 4
-                    SC_count += float(data[1][3:]) * 4
-                elif data[1][0:4] == 'RMB¥':
-                    # it's RMB
-                    SC_count += float(data[1][4:]) * 5
-                else :
-                    SC_count += float(data[1][1:])
-                # record the SC into history
-                with open("DonateHistory.txt", "a") as record:
-                    record.write(i.text)
-                    record.write('\n')
-                    record.write('---------------')
-                    record.write('\n')
-            print('Current Amount: ' + str(SC_count))
-            with open("CurrentDonateAmount.txt", "w") as f:
-                f.write(str(SC_count))
+                    # Check which country's dollar it is.
+                    SC_count += exchange(data[1])
+
+                    # record the SC into history
+                    with open("DonateHistory.txt", "a") as record:
+                        record.write(i.text)
+                        record.write('\n')
+                        record.write('---------------')
+                        record.write('\n')
+                print('Current Amount: ' + str(SC_count))
+                with open("CurrentDonateAmount.txt", "w") as f:
+                    f.write(str(SC_count))
+            else:
+                if data[0] not in unique_chat:
+                    # Unable to differentiate based on messages, so using account names. However, not recording in the history to prevent future identical accounts from submitting without messages.
+                    SC_count += exchange(data[1])
+                    unique_chat.add(data[0])
+
+                    # record the SC into history
+                    with open("DonateHistory.txt", "a") as record:
+                        record.write(i.text)
+                        record.write('\n')
+                        record.write('this is a null SC')
+                        record.write('\n')
+                        record.write('---------------')
+                        record.write('\n')
+
+                    print('Current Amount: ' + str(SC_count))
+                    with open("CurrentDonateAmount.txt", "w") as f:
+                        f.write(str(SC_count))
+                print("This is a null SC.")
         
         color = 'limegreen'
         width =  max(int(345 * SC_count / TARGET), 1)
